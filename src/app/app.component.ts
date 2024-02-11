a02a22552afe7a8c4a9adbd07f147ea07abc0261
Pragyan Deep Gogoi <pdeepgogoi@gmail.com>
2024-02-11 18:41:10 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 
@@ -8,7 +8,7 @@ import { HomeComponent } from "./components/home/home.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'shopcart';
