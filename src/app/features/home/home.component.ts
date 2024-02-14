d7caf8096efff34ed446a946fdd8ae0afc27a8ba
Pragyan Deep Gogoi <pdeepgogoi@gmail.com>
2024-02-14 22:50:47 +0530

    removed console

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -26,9 +26,4 @@ import { CartStoreService } from '../../stores/cart-store/cart-store.service';
 })
 export class HomeComponent {
   public cartStore = inject(CartStoreService)
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    console.log("===")
-  }
 }
